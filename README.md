a60632c5e23b5f30da4727873963da61e0147b2d
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2021-11-27 19:30:49 +0400

    Removed FDO labels from the alg. Added better description

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NISTKDF-800-108-NodeJS
+
 NIST SP800-108 KDFs implementations. Really useful for FIDO Device Onboarding
+
+See section 5.1 in https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-108.pdf
--- index.mjs
+++ index.mjs
@@ -13,9 +13,18 @@ const HashInfo = {
 const FDO_KDF_LABEL   = Buffer.from("FIDO-KDF", "utf-8");
 const FDO_KDF_CONTEXT = Buffer.from("AutomaticOnboardTunnel", "utf-8");
 
-// Implementation of SP800-108 section 5.1 KDF in Counter Mode
-// https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-108.pdf
-export function CounterKDF(sizeBytes, hashAlg, key, contextRand) {
+/**
+ * NIST SP800-108 KDF Counter implementation 
+ * See section 5.1 in https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-108.pdf
+ * @param sizeBytes   - Required size of the output
+ * @param hashAlg     - Hashing algorithm for HMac
+ * @param key         - HMac Key
+ * @param label       - KDF label string. E.g. FDO uses "FDO-KDF" as label
+ * @param context     - KDF context string. E.g. FDO uses "AutomaticOnboardTunnel" as TO2
+ * @param contextRand - Context additional random bytes
+ * @returns 
+ */
+export function CounterKDF(sizeBytes, hashAlg, key, label, context, contextRand) {
     const hInfo = HashInfo[hashAlg];
 
     if (!hInfo) {
@@ -37,9 +46,9 @@ export function CounterKDF(sizeBytes, hashAlg, key, contextRand) {
         const mac = createHmac(hashAlg, key);
 
         mac.update(Buffer.from([i]));
-        mac.update(FDO_KDF_LABEL);
+        mac.update(label);
         mac.update(Buffer.from([0]));
-        mac.update(FDO_KDF_CONTEXT);
+        mac.update(context);
         mac.update(contextRand);
         mac.update(Buffer.from([(l >> 8) & 0xff, l & 0xff]));
 
